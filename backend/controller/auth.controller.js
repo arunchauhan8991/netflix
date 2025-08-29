4b7ca78eae079ed3396c2a1c44981bddb15fa529
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-08-29 20:15:37 +0530

    login and logout functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -70,14 +70,62 @@ export const signup = async (req,res) => {
         console.log("Error in signup controller", error.message);
         return res
           .status(500)
-          .json({ success: false, message: "Internal server error" });
+          .json({ success: false, message: "signup error" });
     }
 }
 
 export const login = async (req,res) => {
-    res.send("login route")
+    try {
+        const {email, password} = req.body
+        if(!email || !password){
+            return res
+              .status(400)
+              .json({ success: false, message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email: email });
+        if (!user) {
+          return res
+            .status(404)
+            .json({ success: false, message: "Invalid credentials" });
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+          return res
+            .status(400)
+            .json({ success: false, message: "Invalid credentials" });
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+          success: true,
+          user: {
+            ...user._doc,
+            password: "",
+          },
+        });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res
+          .status(500)
+          .json({ success: false, message: "login error" });
+    }
 }
 
 export const logout = async (req,res) => {
-    res.send("logout route")
+    try {
+        res.clearCookie("jwt-netflix")
+        res
+        .status(500)
+        .json({ success: true, message: "logged out succesfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res
+          .status(500)
+          .json({ success: false, message: "logout error" });
+    }
 }
